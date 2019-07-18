394f65c29689ca72e4ea6333fae5337991a57163
Bundit Nuntates <silkyland@gmail.com>
2019-07-18 16:06:25 +0700

    add category page

--- routes/admin.js
+++ routes/admin.js
@@ -12,6 +12,58 @@ router.get("/", function(req, res, next) {
   res.render("admin/index");
 });
 
+// หมวดหมู่ข่าว
+
+router.get("/category", (req, res) => {
+  const sql = "SELECT * FROM categories";
+  connection.query(sql, (error, result) => {
+    if (error) return res.send(error.message);
+    res.render("admin/category/index", { categories: result });
+  });
+});
+
+router.get("/category/add", (req, res) => {
+  res.render("admin/category/add");
+});
+
+router.post("/category/create", (req, res) => {
+  const { name } = req.body;
+  const rules = {
+    name: "required|min:3"
+  };
+  const validation = new Validator({ name: name }, rules);
+  if (validation.fails()) {
+    req.flash("error", validation.errors);
+    return res.redirect("/admin/news");
+  }
+  const sql = "INSERT INTO categories (name) VALUES (?)";
+  connection.query(sql, [name], (error, result) => {
+    if (error) return res.send(error.message);
+    res.redirect("/admin/category");
+  });
+});
+
+router.get("/category/edit/:id", (req, res) => {
+  const { id } = req.params;
+  const sql = "SELECT * FROM categories WHERE id = ?";
+  connection.query(sql, [id], (error, response) => {
+    if (error) return res.send(error.message);
+    res.render("/admin/edit", { category: result });
+  });
+});
+
+router.get("/category/delete/:id", (req, res) => {
+  const { id } = req.params;
+  const sql = "DELETE FROM categories WHERE id = ?";
+  connection.query(sql, [id], (error, result) => {
+    if (error) return res.send(error.message);
+    req.flash("success", "ลบข้อมูลสำเร็จแล้ว");
+    res.redirect("/admin/category");
+  });
+});
+
+// ข่าว
+
 router.get("/news", (req, res, next) => {
   res.render("admin/news/index");
 });
@@ -25,14 +77,20 @@ router.get("/news/add", (req, res, next) => {
 });
 
 router.post("/news/admin/create", (req, res) => {
-  const rules = {};
+  const rules = {
+    name: "require|min:3"
+  };
   const validation = new Validator(req.body, rules);
   if (validation.fails()) {
     req.flash("error", validation.errors);
     return res.redirect("/admin/news");
   }
-  const sql = "INSERT INTO news () VALUES (?, ?)"; //TODO: Edit this line please
-  res.redirect("/admin/news");
+  const sql = "INSERT INTO news (name) VALUES (?)";
+  connection.query(sql, [req.body.name], (error, result) => {
+    if (error) return res.send(error.message);
+    req.flash("success", { message: "บันทึกข้อมูลสำเร็จ" });
+    res.redirect("/admin/news");
+  });
 });
 
 router.get("/news/edit/:id", (req, res) => {
