dc7b813243d1604f8591568ec34858261c6a3cf4
Bundit Nuntates <silkyland@gmail.com>
2021-03-14 23:43:58 +0700

    add docker-compose

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ import mysql from "mysql";
 
 const connection = mysql.createConnection({
   multipleStatements: true,
-  host: "localhost",
+  host: "mariadb",
   user: "root",
   password: "1234",
   database: "webwat"
