d1cc4245310974a491b7329892f3fd84ff76cc15
Bundit Nuntates <silkyland@gmail.com>
2021-03-21 16:28:56 +0700

    update

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ import mysql from "mysql";
 
 const connection = mysql.createConnection({
   multipleStatements: true,
-  host: "178.128.208.60",
+  host: "host.docker.internal",
   user: "root",
   password: "O4504Umq8y5hzcR2",
   database: "webwat"
